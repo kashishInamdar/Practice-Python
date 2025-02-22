3f8e29b9be230b8c2007f2ab9b177c39450da62f
kashishInamdar <139747921+kashishInamdar@users.noreply.github.com>
2025-02-22 23:20:25 +0530

    Update function.py

--- function.py
+++ function.py
@@ -58,3 +58,14 @@ def factorial(n):
 # Calling the recursive function
 number = 5
 print(f"Factorial of {number} is:", factorial(number))
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
